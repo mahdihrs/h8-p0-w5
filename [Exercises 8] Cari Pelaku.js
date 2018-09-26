cb2347e33e0d10b55cad5584296062678c9fe37f
Mahdihrs <mahdiharis@outlook.com>
2018-09-26 17:28:22 +0700

    berlatih menggunakan regular expressions

---
+++ [Exercises 8] Cari Pelaku.js
@@ -0,0 +1,11 @@
+function cariPelaku(str) {
+  var output = str.match(/abc/g)
+  return output.length;
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
